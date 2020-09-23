342c52df3da6a8d888e3f365ec6774595b9722ef
Leo Barath <lbarath@live.se>
2020-09-23 13:35:12 +0200

    Första puzzel

---
+++ Alloy/Assets/Scripts/Scripts_Puzzle_01/Open_Door_Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Door_Script : MonoBehaviour
+{
+    public bool doorIsOpen = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Stand_On_Button_Script.buttonIsPressed == 2)
+        {
+            doorIsOpen = true;
+        }
+    }
+}
---
+++ Alloy/Assets/Scripts/Scripts_Puzzle_01/Stand_On_Button_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stand_On_Button_Script : MonoBehaviour
+{
+    public static int buttonIsPressed;
+    private bool buttonIsPressedBool = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonIsPressed = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && buttonIsPressedBool == false)
+        {
+            buttonIsPressed += 1;
+            buttonIsPressedBool = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && buttonIsPressedBool == true)
+        {
+            buttonIsPressed -= 1;
+            buttonIsPressedBool = false;
+        }
+    }
+}
