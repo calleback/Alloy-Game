5d601fdac4a5d28ff51765912e63d08acd2efa4d
xRap1d <71003166+xRap1d@users.noreply.github.com>
2020-10-03 12:35:54 +0200

    Extremt basic pickup

--- Alloy/Assets/Scripts/ButtonOpening.cs
+++ Alloy/Assets/Scripts/ButtonOpening.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarlButtonOpenDoor : MonoBehaviour
+public class ButtonOpening : MonoBehaviour
 {
     public GameObject button;
     public GameObject door;
 
-    public Transform doorFinalPos;
-    public Transform doorStartingPos;
+    public Transform doorStartPos;
+    public Transform doorEndPos;
 
     public Transform buttonStartPos;
     public Transform buttonEndPos;
@@ -27,27 +27,27 @@ public class CarlButtonOpenDoor : MonoBehaviour
         //if button is pressed, opening animation plays.
         if (buttonIsPressed)
         {
-            door.transform.position = Vector3.MoveTowards(door.transform.position, doorFinalPos.position, stepDoor);
+            door.transform.position = Vector3.MoveTowards(door.transform.position, doorEndPos.position, stepDoor);
             button.transform.position = Vector3.MoveTowards(button.transform.position, buttonEndPos.position, stepButton);
         }
         //if button is not pressed, return door and button to original position
         if (!buttonIsPressed)
         {
-            door.transform.position = Vector3.MoveTowards(door.transform.position, doorStartingPos.position, stepDoor);
+            door.transform.position = Vector3.MoveTowards(door.transform.position, doorStartPos.position, stepDoor);
             button.transform.position = Vector3.MoveTowards(button.transform.position, buttonStartPos.position, stepButton);
         }
     }
     //if the player touches the button buttonIsPressed bool is set to true
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && !buttonIsPressed)
+        if (other.tag == "Player" && !buttonIsPressed || other.tag == "InteractObject" && !buttonIsPressed)
         {
             buttonIsPressed = true;
         }
     }
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player" && buttonIsPressed)
+        if (other.tag == "Player" && buttonIsPressed || other.tag == "InteractObject" && buttonIsPressed)
         {
             buttonIsPressed = false;
         }
