16067872800e07f0d64be53dcfdd60db8e32fdff
xRap1d <71003166+xRap1d@users.noreply.github.com>
2020-10-21 15:53:15 +0200

    Fixade lite raycast grejor

---
+++ Alloy/Assets/Scripts/PickUp/RayCastTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTest : MonoBehaviour
+{
+    public Camera cameraMain;
+    public float crosshairRange;
+    public float pickupRange;
+    void Update()
+    {
+        RayCastCheck();
+    }
+    void RayCastCheck()
+    {
+        RaycastHit hit;
+        Ray ray = cameraMain.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 3000, Color.red);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "IntObj")
+            {
+                //draw crosshair
+            }
+            if (hit.distance <= pickupRange)
+            {
+                //draw pickup crosshair and allow player to pick up object
+            }
+        }
+    }
+}
