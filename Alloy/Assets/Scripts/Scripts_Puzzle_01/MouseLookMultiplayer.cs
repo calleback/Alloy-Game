937b488e0840321fa73a326c99d431ffeb553dba
Leo Barath <lbarath@live.se>
2020-10-21 16:33:38 +0200

    Fixat multiplayer movement

---
+++ Alloy/Assets/Scripts/Scripts_Puzzle_01/MouseLookMultiplayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MouseLookMultiplayer : NetworkBehaviour
+{
+    public float mouseSensitivty = 100f;
+
+    public Transform playerBody;
+
+    public bool isFrozen = false;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivty * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivty * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+
+            //if (Input.GetKeyDown(KeyCode.Escape) && !isFrozen)
+            //{
+            //    Time.timeScale = 0;
+            //    mouseSensitivty = 0;
+            //    isFrozen = true;
+            //}
+            //else if (Input.GetKeyDown(KeyCode.Escape) && isFrozen)
+            //{
+            //    Time.timeScale = 1;
+            //    mouseSensitivty = 100f;
+            //    isFrozen = false;
+            //}
+        }
+
+    }
+}
