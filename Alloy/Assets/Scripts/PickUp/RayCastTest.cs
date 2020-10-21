4097a422db60260d5d63fb3d9e0fbeee80b51c29
xRap1d <71003166+xRap1d@users.noreply.github.com>
2020-10-21 16:36:57 +0200

    TEST123

--- Alloy/Assets/Scripts/PickUp/RayCastTest.cs
+++ Alloy/Assets/Scripts/PickUp/RayCastTest.cs
@@ -8,6 +8,8 @@ public class RayCastTest : MonoBehaviour
     public Camera cameraMain;
     public float pickupRange;
 
+    public Transform rightHand;
+
     //används inte än
     private float fadeRate = 1f;
     private Image image;
@@ -46,6 +48,24 @@ public class RayCastTest : MonoBehaviour
             {
                 defaultXhair.SetActive(false);
                 pickupXhair.SetActive(false);
+            }
+            if (hit.transform.tag == "IntObj" && hit.distance < pickupRange && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Transform tempHold = hit.transform;
+
+                hit.transform.GetComponent<Rigidbody>().useGravity = false;
+                hit.transform.GetComponent<Rigidbody>().freezeRotation = true;
+                hit.transform.GetComponent<Rigidbody>().isKinematic = true;
+                hit.transform.position = rightHand.position;
+                hit.transform.parent = GameObject.Find("HoldObject").transform;
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    tempHold.transform.parent = null;
+                    tempHold.GetComponent<Rigidbody>().useGravity = true;
+                    tempHold.GetComponent<Rigidbody>().freezeRotation = false;
+                    tempHold.GetComponent<Rigidbody>().isKinematic = false;
+                }
             }
         }
     }
