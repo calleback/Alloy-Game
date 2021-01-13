7e7c7c3721eecb6f3baba74f55d251adce905306
Leo Barath <lbarath@live.se>
2021-01-13 09:51:18 +0100

    Meny och quit button

---
+++ Alloy/Assets/Scripts/Menu/Quit_Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_Menu : MonoBehaviour
+{
+    public void Quit_Game()
+    {
+        Application.Quit(); 
+    }
+}
