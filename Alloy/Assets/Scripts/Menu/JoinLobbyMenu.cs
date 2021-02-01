b8f9fda6451b70ff1583f93067d992514060a51d
Leo Barath <lbarath@live.se>
2021-02-01 12:37:38 +0100

    MainMEnu

---
+++ Alloy/Assets/Scripts/Menu/JoinLobbyMenu.cs
@@ -0,0 +1,33 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
+//using UnityEngine.UI;
+
+//public class JoinLobbyMenu : MonoBehaviour
+//{
+//    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+//    [Header("UI")]
+//    [SerializeField] private GameObject landingPanel = null;
+//    [SerializeField] private TMP_InputField ipAddressInputField = null;
+//    [SerializeField] private Button JoinButton = null;
+
+//    //private void OnEnable()
+//    //{
+//    //    NetworkManagerLobby.OnClientConnected += HandleClie
+//    //}
+
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
