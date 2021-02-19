8486271330ddd99c996d816839e905544b60c1d0
xRap1d <71003166+xRap1d@users.noreply.github.com>
2021-02-19 16:58:25 +0100

    Början på meny

---
+++ Alloy/Assets/AI_Follow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Follow : MonoBehaviour
+{
+    Transform player;
+
+    Vector3 playerTransform;
+
+    private bool chasePlayer;
+
+    double chasePlayerRange = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        chasePlayer = false;
+
+        gameObject.GetComponent<NavMeshAgent>().speed = 100f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(player.position, transform.position) < chasePlayerRange)
+        {
+            chasePlayer = true;
+        }
+        if (Vector3.Distance(player.position, transform.position) > chasePlayerRange)
+        {
+            chasePlayer = false;
+        }
+    }
+    private void Update()
+    {
+        if (chasePlayer == true)
+        {
+            gameObject.GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
+        }
+    }
+}
