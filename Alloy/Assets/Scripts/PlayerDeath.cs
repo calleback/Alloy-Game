2b5d379ef1ab5b0c7fbbf264f7587831cf83bbd3
xRap1d <71003166+xRap1d@users.noreply.github.com>
2021-02-21 18:42:25 +0100

    Death

---
+++ Alloy/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    Transform player;
+    private void Start()
+    {
+        player = GameObject.Find("FirstPersonCharacter").transform;
+    }
+    void Update()
+    {
+        if (player.transform.position.y < -10)
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Time.timeScale = 1f;
+            FindObjectOfType<MouseLook>().mouseSensitivty = 100f;
+        }
+    }
+}
